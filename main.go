a4f027b88a5e1daf936766e4184a0ecfd7f1ad9b
Victor Biga <victor.biga@gmail.com>
2024-05-23 19:28:46 +0200

    feat: implement readiness and liveness probes

--- main.go
+++ main.go
@@ -37,38 +37,45 @@ func init() {
 func main() {
     // Handle POST requests to update metrics
     http.HandleFunc("/update", func(w http.ResponseWriter, r *http.Request) {
-        var data struct {
-            Number       float64 `json:"number"`
-            Organization string  `json:"organization"`
-            Repository   string  `json:"repository"`
-            Metric       string  `json:"metric"`
-        }
-        err := json.NewDecoder(r.Body).Decode(&data)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
-        if data.Organization == "" || data.Repository == "" || data.Metric == "" {
-            http.Error(w, "Missing 'organization', 'repository', or 'metric' field", http.StatusBadRequest)
-            return
-        }
-
-        switch data.Metric {
-        case "terraform":
-            terraformResourcesCount.WithLabelValues(data.Organization, data.Repository).Set(data.Number)
-        case "cnrm":
-            cnrmResourcesCount.WithLabelValues(data.Organization, data.Repository).Set(data.Number)
-        default:
-            http.Error(w, "Invalid 'metric' field value", http.StatusBadRequest)
-            return
-        }
-
-        w.WriteHeader(http.StatusOK)
-        w.Write([]byte("Metric updated"))
+        updateMetric(w, r, terraformResourcesCount)
     })
 
     // Expose the custom registry metrics via HTTP
     http.Handle("/metrics", promhttp.HandlerFor(customRegistry, promhttp.HandlerOpts{}))
 
+    // Liveness probe
+    http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("ok"))
+    })
+
+    // Readiness probe
+    http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
+        // Check if the application is ready to serve traffic
+        // Implement your readiness checks here
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("ready"))
+    })
+
     log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func updateMetric(w http.ResponseWriter, r *http.Request, metric *prometheus.GaugeVec) {
+    var data struct {
+        Number       float64 `json:"number"`
+        Organization string  `json:"organization"`
+        Repository   string  `json:"repository"`
+    }
+    err := json.NewDecoder(r.Body).Decode(&data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+    if data.Organization == "" || data.Repository == "" {
+        http.Error(w, "Missing 'organization' or 'repository' field", http.StatusBadRequest)
+        return
+    }
+    metric.WithLabelValues(data.Organization, data.Repository).Set(data.Number)
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("Metric updated"))
 }
