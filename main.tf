4fde523299b3bc4f0cf0cd7dd837f28b5ebd7d60
Victor Biga <victor.biga@gmail.com>
2024-08-24 20:59:50 +0200

    🐛 (main.tf): delete resource block for PagerDuty service example as it is no longer needed in the configuration file

--- main.tf
+++
@@ -1,6 +0,0 @@
-resource "pagerduty_service" "example" {
-  name              = "example"
-  escalation_policy = pagerduty_escalation_policy.example.id
-  type              = "generic_events"
-  description       = "Example service"
-}
